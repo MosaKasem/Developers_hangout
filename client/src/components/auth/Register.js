af1cd3066e85f710011ce95736f414da83de8e86
MosaKasem <thescorpion119@gmail.com>
2019-06-20 16:49:41 +0200

    register complete - frontend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import axios from 'axios'
 const Register = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -12,8 +13,30 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault()
+    if (password !== password2) {
+      console.log('Passwords do not match')
+    } else {
+      const newUser = {
+        name: name,
+        email: email,
+        password: password
+      }
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser)
+        const res = await axios.post('/api/users', body, config)
+        console.log('res: ', res)
+      } catch (error) {
+        console.log(error)
+        console.log(error.response.data)
+      }
+    }
   }
   return (
     <Fragment>
