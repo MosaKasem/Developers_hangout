d95828a0900053f4f153255a1fad5cc16ddb6c99
MosaKasem <thescorpion119@gmail.com>
2019-06-21 22:45:41 +0200

    added flash messages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+import Alert from './components/layout/Alert'
 import './App.css'
 
 // Redux>
@@ -13,12 +14,12 @@ import store from './store'
 
 const App = () =>
   <Provider store={store}>
-
     <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
---
+++ client/src/actions/alert.js
@@ -0,0 +1,15 @@
+import { SET_ALERT, REMOVE_ALERT } from './types'
+import uuid from 'uuid'
+
+export const setAlert = (message, alertType, timeout = 9000) => dispatch => {
+  const id = uuid.v4()
+  dispatch({
+    type: SET_ALERT,
+    payload: {
+      message, alertType, id
+    }
+  })
+  setTimeout(() => dispatch({
+    type: REMOVE_ALERT, payload: id
+  }), timeout)
+}
