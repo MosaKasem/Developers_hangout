df042a3b58b9d8de208281a35674dd94e433096d
MosaKasem <thescorpion119@gmail.com>
2019-06-23 20:25:42 +0200

    added component dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,25 @@
- import React from 'react'
- import PropTypes from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
+import auth from '../../reducers/auth'
 
- const DashBoard = props => {
-     return (
-         <div>
-             DashBoard
+const DashBoard = props => {
+  return (
+    <div>
+            DashBoard
          </div>
-     )
- }
- DashBoard.prototype = {
+  )
+}
+DashBoard.prototype = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
 
- }
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+})
 
- export default DashBoard
+export default connect(mapStateToProps, { getCurrentProfile })(DashBoard)
