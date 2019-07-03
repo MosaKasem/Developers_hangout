78cdd63a137b836ece61c150ed007d3a41db8258
MosaKasem <thescorpion119@gmail.com>
2019-07-04 00:04:58 +0200

    added profile bio for users

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import { getProfileByID } from '../../actions/profile'
 
 const Profile = ({
@@ -13,7 +16,26 @@ const Profile = ({
     getProfileByID(match.params.id)
   }, [getProfileByID, match.params.id])
   return (
-    <div />
+    <Fragment>
+      { profile === null || loading ? (<Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>Back to profile</Link>
+          { auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id &&
+            (
+            <Link to='/edit-profile' className='btn btn-dark'>
+            Edit Profile
+            </Link>
+            )}
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
+        </Fragment>
+        )}
+    </Fragment>
   )
 }
 
