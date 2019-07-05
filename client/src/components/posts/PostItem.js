a47acafe8f441f1493620a5481a4803c73e59509
MosaKasem <thescorpion119@gmail.com>
2019-07-06 00:04:45 +0200

    posts now show up on frontend

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -13,37 +13,38 @@ function PostItem ({ auth, post: { _id, text, name, avatar, user, likes, comment
             <a href='profile.html'>
               <img
                 className='round-img'
-                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
+                src={avatar}
                 alt=''
             />
-              <h4>John Doe</h4>
+              <h4>{name}</h4>
             </a>
           </div>
           <div>
             <p className='my-1'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-              possimus corporis sunt necessitatibus! Minus nesciunt soluta
-              suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-              dolor? Illo perferendis eveniet cum cupiditate aliquam?
+              {text}
             </p>
             <p className='post-date'>
-                Posted on 04/16/2019
+                Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
             <button type='button' className='btn btn-light'>
               <i className='fas fa-thumbs-up' />
-              <span>4</span>
+              <span>{likes.length}</span>
             </button>
             <button type='button' className='btn btn-light'>
               <i className='fas fa-thumbs-down' />
             </button>
-            <a href='post.html' className='btn btn-primary'>
-              Discussion <span className='comment-count'>2</span>
-            </a>
+            <Link to={`/post/${_id}`} className='btn btn-primary'>
+              Discussion {comments.length > 0 && (
+              <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
             <button
               type='button'
               className='btn btn-danger'>
               <i className='fas fa-times' />
             </button>
+            )}
           </div>
         </div>
       </div>
