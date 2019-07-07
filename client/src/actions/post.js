787e5ce1fd49d13b8af1de18c86928b83c22896e
MosaKasem <thescorpion119@gmail.com>
2019-07-07 17:51:06 +0200

    addPost form showing, minor error, addPost undefined

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_POST, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types'
+import { GET_POST, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types'
 
 // GET posts
 export const getPosts = () => async dispatch => {
@@ -56,14 +56,33 @@ export const removeLike = id => async dispatch => {
 // DELETE post
 export const deletePost = id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`)
-    console.log('res: ', res)
-    console.log('What the fuck')
+    await axios.delete(`/api/posts/${id}`)
     dispatch({
       type: DELETE_POST,
       payload: id
     })
     dispatch(setAlert('Post Removed', 'success'))
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    })
+  }
+}
+
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  try {
+    const res = await axios.post('/api/posts/', formData, config)
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    })
+    dispatch(setAlert('Post Created', 'success'))
   } catch (error) {
     dispatch({
       type: POST_ERROR,
