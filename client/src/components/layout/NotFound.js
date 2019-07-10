01faabb6c208f05d0da5931fb11b4dcf64b6219a
MosaKasem <thescorpion119@gmail.com>
2019-07-10 12:43:31 +0200

    added page not found

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className='x-large text-primary'>
+        <i className='fas fa-exclamation-triangle' /> This Page mhmm..
+      </h1>
+      <p>This page does not exists</p>
+    </Fragment>
+  )
+}
+
+export default NotFound
